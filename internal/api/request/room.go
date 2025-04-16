7fa6aeaecba80f0be09f7a3addaade0fafd9c4f3
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-16 22:23:51 +0700

    added struct tags for binding

--- internal/api/request/room.go
+++ internal/api/request/room.go
@@ -1,13 +1,13 @@
 package request
 
 type CreateRoom struct {
-	RoomNumber     string `json:"roomNumber" validate:"required"`
-	TreatmentClass string `json:"treatmentClass" validate:"required"`
-	RoomTypeID     uint   `json:"roomTypeID" validate:"required"`
+	RoomNumber     string `json:"roomNumber" binding:"required" validate:"required"`
+	TreatmentClass string `json:"treatmentClass" binding:"required" validate:"required"`
+	RoomTypeID     uint   `json:"roomTypeID" binding:"required" validate:"required"`
 }
 
 type UpdateRoom struct {
-	RoomNumber     string `json:"roomNumber" validate:"required"`
-	TreatmentClass string `json:"treatmentClass" validate:"required"`
-	RoomTypeID     uint   `json:"roomTypeID" validate:"required"`
+	RoomNumber     string `json:"roomNumber" binding:"required" validate:"required"`
+	TreatmentClass string `json:"treatmentClass" binding:"required" validate:"required"`
+	RoomTypeID     uint   `json:"roomTypeID" binding:"required" validate:"required"`
 }
--- internal/api/request/roomtype.go
+++ internal/api/request/roomtype.go
@@ -1,9 +1,9 @@
 package request
 
 type CreateRoomType struct {
-	Name string `json:"name" validate:"required"`
+	Name string `json:"name" binding:"required" validate:"required"`
 }
 
 type UpdateRoomType struct {
-	Name string `json:"name" validate:"required"`
+	Name string `json:"name" binding:"required" validate:"required"`
 }
