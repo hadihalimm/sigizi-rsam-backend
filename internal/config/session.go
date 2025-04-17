265c14e8ff72200f1136143f3816bd281b510f5d
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-17 14:01:08 +0700

    add middleware

--- internal/config/session.go
+++ internal/config/session.go
@@ -1,8 +1,10 @@
 package config
 
 import (
+	"net/http"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 )
 
@@ -13,5 +15,28 @@ func init() {
 		Path:     "/",
 		MaxAge:   int((12 * time.Hour).Seconds()),
 		HttpOnly: true,
+	}
+}
+
+func SessionMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session, err := SessionStore.Get(c.Request, "sigizi-session")
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Failed to load session",
+			})
+			return
+		}
+
+		userID, ok := session.Values["userID"].(uint)
+		if !ok || userID == 0 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+				"error": "Invalid session or no user_id",
+			})
+			return
+		}
+
+		c.Set("userID", userID)
+		c.Next()
 	}
 }
