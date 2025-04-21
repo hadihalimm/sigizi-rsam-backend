c2b3adc49c9fd7e006bc5de7cba43f8c312b56dc
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-21 14:03:30 +0700

    remove Date property and added createdAt and updatedAt

--- internal/model/dailypatientmeal.go
+++ internal/model/dailypatientmeal.go
@@ -4,9 +4,13 @@ import "time"
 
 type DailyPatientMeal struct {
 	ID         uint      `gorm:"primaryKey" json:"id"`
-	Date       time.Time `gorm:"not null;index" json:"date"`
 	PatientID  uint      `gorm:"not null;index" json:"patientID"`
 	RoomID     uint      `gorm:"not null;index" json:"roomID"`
 	MealTypeID uint      `gorm:"not null;index" json:"mealTypeID"`
 	Notes      string    `json:"notes"`
+	CreatedAt  time.Time `json:"createdAt"`
+	UpdatedAt  time.Time `json:"updatedAt"`
+	Patient    Patient   `json:"patient"`
+	Room       Room      `json:"room"`
+	MealType   MealType  `json:"mealType"`
 }
