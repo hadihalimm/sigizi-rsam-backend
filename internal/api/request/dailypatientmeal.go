9b9d38965a6977b26adcbaa38ee6cb3cb6881ab4
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-21 14:02:36 +0700

    removed Date input

--- internal/api/request/dailypatientmeal.go
+++ internal/api/request/dailypatientmeal.go
@@ -1,19 +1,15 @@
 package request
 
-import "time"
-
 type CreateDailyPatientMeal struct {
-	Date       time.Time `json:"date" binding:"required" validate:"required"`
-	PatientID  uint      `json:"patientID" binding:"required" validate:"required"`
-	RoomID     uint      `json:"roomID" binding:"required" validate:"required"`
-	MealTypeID uint      `json:"mealTypeID" binding:"required" validate:"required"`
-	Notes      string    `json:"notes"`
+	PatientID  uint   `json:"patientID" binding:"required" validate:"required"`
+	RoomID     uint   `json:"roomID" binding:"required" validate:"required"`
+	MealTypeID uint   `json:"mealTypeID" binding:"required" validate:"required"`
+	Notes      string `json:"notes"`
 }
 
 type UpdateDailyPatientMeal struct {
-	Date       time.Time `json:"date" binding:"required" validate:"required"`
-	PatientID  uint      `json:"patientID" binding:"required" validate:"required"`
-	RoomID     uint      `json:"roomID" binding:"required" validate:"required"`
-	MealTypeID uint      `json:"mealTypeID" binding:"required" validate:"required"`
-	Notes      string    `json:"notes"`
+	PatientID  uint   `json:"patientID" binding:"required" validate:"required"`
+	RoomID     uint   `json:"roomID" binding:"required" validate:"required"`
+	MealTypeID uint   `json:"mealTypeID" binding:"required" validate:"required"`
+	Notes      string `json:"notes"`
 }
