1de17330abcb31a9d20b356ec7ddf387370cba8f
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-04-23 15:27:49 +0700

    add json tag

--- internal/model/patient.go
+++ internal/model/patient.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Patient struct {
-	ID                  uint      `gorm:"primaryKey"`
-	MedicalRecordNumber string    `gorm:"unique;not null"`
-	Name                string    `gorm:"not null"`
-	DateOfBirth         time.Time `gorm:"not null"`
+	ID                  uint      `gorm:"primaryKey" json:"id"`
+	MedicalRecordNumber string    `gorm:"unique;not null" json:"medicalRecordNumber"`
+	Name                string    `gorm:"not null" json:"name"`
+	DateOfBirth         time.Time `gorm:"not null" json:"dateOfBirth"`
 }
