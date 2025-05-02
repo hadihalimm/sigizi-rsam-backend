3b86abf817623ba8c26560ba54c5d49de849d570
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-02 09:23:08 +0700

    add date attribute

--- internal/api/request/dailypatientmeal.go
+++ internal/api/request/dailypatientmeal.go
@@ -1,11 +1,14 @@
 package request
 
+import "time"
+
 type CreateDailyPatientMeal struct {
-	PatientID  uint   `json:"patientID" binding:"required" validate:"required"`
-	RoomID     uint   `json:"roomID" binding:"required" validate:"required"`
-	MealTypeID uint   `json:"mealTypeID" binding:"required" validate:"required"`
-	Notes      string `json:"notes"`
-	DietIDs    []uint `json:"dietIDs"`
+	PatientID  uint      `json:"patientID" binding:"required" validate:"required"`
+	RoomID     uint      `json:"roomID" binding:"required" validate:"required"`
+	MealTypeID uint      `json:"mealTypeID" binding:"required" validate:"required"`
+	Date       time.Time `json:"date" binding:"required" validate:"required"`
+	Notes      string    `json:"notes"`
+	DietIDs    []uint    `json:"dietIDs"`
 }
 
 type UpdateDailyPatientMeal struct {
