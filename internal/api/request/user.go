042cae3d4f8ab4a0251066e5e60adfe5c3e5fae2
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-06 09:59:34 +0700

    request structs for User

---
+++ internal/api/request/user.go
@@ -0,0 +1,11 @@
+package request
+
+type UpdateUser struct {
+	Username string `json:"username" validate:"required,min=4,max=32"`
+	Name     string `json:"name" validate:"required,min=1,max=64"`
+	Role     string `json:"role" validate:"required,min=1,max=32"`
+}
+
+type UpdatePassword struct {
+	Password string `json:"password" validate:"required,min=8,max=32"`
+}
