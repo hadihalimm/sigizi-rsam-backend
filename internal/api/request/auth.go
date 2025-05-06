e80dda2cbeb6102b8b36868d8638b93d18d6dc7d
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-06 09:59:48 +0700

    add Name attribute to Register request

--- internal/api/request/auth.go
+++ internal/api/request/auth.go
@@ -2,6 +2,7 @@ package request
 
 type Register struct {
 	Username string `json:"username" validate:"required,min=4,max=32"`
+	Name     string `json:"name" validate:"required,min=1,max=64"`
 	Password string `json:"password" validate:"required,min=8,max=32"`
 	Role     string `json:"role" validate:"required,min=1,max=32"`
 }
