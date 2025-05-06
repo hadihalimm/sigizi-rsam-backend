ad435994e8e988dec202c455378c28b9235f7fca
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-06 09:58:20 +0700

    add name and change role attribute type

--- internal/model/user.go
+++ internal/model/user.go
@@ -5,8 +5,9 @@ import "time"
 type User struct {
 	ID           uint      `gorm:"primaryKey" json:"id"`
 	Username     string    `gorm:"unique;not null" json:"username"`
+	Name         string    `gorm:"not null" json:"name"`
 	PasswordHash string    `gorm:"not null" json:"-"`
-	Role         string    `gorm:"not null" json:"role"`
+	Role         string    `gorm:"type:ENUM('perawat','ahli_gizi','admin');not null" json:"role"`
 	CreatedAt    time.Time `json:"createdAt"`
 	UpdatedAt    time.Time `json:"updatedAt"`
 }
