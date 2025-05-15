033f4b817cb14f79ac5acefcff46d158abf1c76c
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-15 15:51:02 +0700

    remove SessionMiddleware

--- internal/config/session.go
+++ internal/config/session.go
@@ -4,39 +4,16 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gin-gonic/gin"
 	"github.com/gorilla/sessions"
 )
 
 var SessionStore = sessions.NewCookieStore([]byte("tes123"))
 
-func init() {
+func InitSession() {
 	SessionStore.Options = &sessions.Options{
 		Path:     "/",
-		MaxAge:   int((12 * time.Hour).Seconds()),
+		MaxAge:   int((8 * time.Hour).Seconds()),
 		HttpOnly: true,
-	}
-}
-
-func SessionMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session, err := SessionStore.Get(c.Request, "sigizi-session")
-		if err != nil {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Failed to load session",
-			})
-			return
-		}
-
-		userID, ok := session.Values["userID"].(uint)
-		if !ok || userID == 0 {
-			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Invalid session or no user_id",
-			})
-			return
-		}
-
-		c.Set("userID", userID)
-		c.Next()
+		SameSite: http.SameSiteNoneMode,
 	}
 }
