d00c2d54d0842ad0a078ad7390c2dacd89dc15a3
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-18 21:29:54 +0700

    add session middleware

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,19 +1,22 @@
 package api
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/hadihalimm/sigizi-rsam/internal/config"
 )
 
 func (s *Server) RequireSession(c *gin.Context) {
-	// session, err := config.SessionStore.Get(c.Request, "sigizi-rsam")
-	// if err != nil || session.Values["userID"] == nil {
-	// 	c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-	// 		"error": "Session invalid or expired",
-	// 	})
-	// 	return
-	// }
+	session, err := config.SessionStore.Get(c.Request, "sigizi-rsam")
+	if err != nil || session.Values["userID"] == nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"error": "Session invalid or expired",
+		})
+		return
+	}
 
-	// userID := session.Values["userID"].(uint)
-	// c.Set("userID", userID)
+	userID := session.Values["userID"].(uint)
+	c.Set("userID", userID)
 	c.Next()
 }
