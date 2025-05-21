3ce89d0474299eab27a8b81ad5a0080eb67de0c1
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-05-21 13:43:24 +0700

    add dsn for external hosting

--- internal/config/database.go
+++ internal/config/database.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"fmt"
 	"log"
-	"net/url"
-	"os"
 
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -21,18 +18,17 @@ func ConnectToDatabase() *Database {
 		return dbInstance
 	}
 
-	DB_HOST := os.Getenv("DB_HOST")
-	DB_PORT := os.Getenv("DB_PORT")
-	DB_NAME := os.Getenv("DB_NAME")
-	DB_USER := os.Getenv("DB_USER")
-	DB_PASSWORD := os.Getenv("DB_PASSWORD")
-	loc := url.QueryEscape("Asia/Jakarta")
+	// DB_HOST := os.Getenv("DB_HOST")
+	// DB_PORT := os.Getenv("DB_PORT")
+	// DB_NAME := os.Getenv("DB_NAME")
+	// DB_USER := os.Getenv("DB_USER")
+	// DB_PASSWORD := os.Getenv("DB_PASSWORD")
+	// loc := url.QueryEscape("Asia/Jakarta")
 
-	// dsnLocal := "hadi:hadi@tcp(127.0.0.1:3306)/sigizi_rsam?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s",
-		DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, loc)
-	fmt.Println(dsn)
-	gormDB, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsnLocal := "hadi:hadi@tcp(127.0.0.1:3306)/sigizi_rsam?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=%s",
+	// 	DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, loc)
+	gormDB, err := gorm.Open(mysql.Open(dsnLocal), &gorm.Config{})
 	if err != nil {
 		log.Fatal(err)
 	}
