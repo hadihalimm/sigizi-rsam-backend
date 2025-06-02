1df3cf85f80018ed05427ce7563d40a2980d6cfe
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-02 11:45:45 +0700

    add code field

--- internal/api/request/room.go
+++ internal/api/request/room.go
@@ -2,12 +2,14 @@ package request
 
 type CreateRoom struct {
 	Name           string `json:"name" binding:"required" validate:"required"`
+	Code           string `json:"code" binding:"required" validate:"required"`
 	TreatmentClass string `json:"treatmentClass" binding:"required" validate:"required"`
 	RoomTypeID     uint   `json:"roomTypeID" binding:"required" validate:"required"`
 }
 
 type UpdateRoom struct {
 	Name           string `json:"name" binding:"required" validate:"required"`
+	Code           string `json:"code" binding:"required" validate:"required"`
 	TreatmentClass string `json:"treatmentClass" binding:"required" validate:"required"`
 	RoomTypeID     uint   `json:"roomTypeID" binding:"required" validate:"required"`
 }
