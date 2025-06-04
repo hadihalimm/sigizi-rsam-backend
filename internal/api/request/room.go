62e543a5feebc4ae74f0a495b185c0b64a5c6e1e
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-04 09:25:34 +0700

    add new property

--- internal/api/request/room.go
+++ internal/api/request/room.go
@@ -3,6 +3,7 @@ package request
 type CreateRoom struct {
 	Name           string `json:"name" binding:"required" validate:"required"`
 	Code           string `json:"code" binding:"required" validate:"required"`
+	ClassID        string `json:"treatment_class_id" binding:"required" validate:"required"`
 	TreatmentClass string `json:"treatmentClass" binding:"required" validate:"required"`
 	RoomTypeID     uint   `json:"roomTypeID" binding:"required" validate:"required"`
 }
@@ -10,6 +11,7 @@ type CreateRoom struct {
 type UpdateRoom struct {
 	Name           string `json:"name" binding:"required" validate:"required"`
 	Code           string `json:"code" binding:"required" validate:"required"`
+	ClassID        string `json:"treatment_class_id" binding:"required" validate:"required"`
 	TreatmentClass string `json:"treatmentClass" binding:"required" validate:"required"`
 	RoomTypeID     uint   `json:"roomTypeID" binding:"required" validate:"required"`
 }
