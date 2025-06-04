0c68c99ff7edaa0ec0164cdbb0498c25fe28b457
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-04 15:10:21 +0700

    drop table initialization

--- internal/api/server.go
+++ internal/api/server.go
@@ -43,7 +43,7 @@ func NewServer() *http.Server {
 	// 	&model.RoomType{}, &model.Room{},
 	// 	&model.Food{}, &model.MealType{}, &model.MealItem{},
 	// 	&model.Patient{}, &model.DailyPatientMeal{})
-	// db.Gorm.Migrator().DropTable(&model.RoomType{}, &model.Room{})
+	// db.Gorm.Migrator().DropTable(&model.Room{}, &model.RoomType{}, &model.DailyPatientMeal{}, &model.DailyPatientMealLog{})
 
 	db.Gorm.AutoMigrate(&model.User{},
 		&model.RoomType{}, &model.Room{},
