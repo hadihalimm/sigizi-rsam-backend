33c23a66d7d5ef38e387ebb2f76dbb151b727b0d
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-10 08:20:48 +0700

    add logging

--- internal/handler/dailypatientmeal.go
+++ internal/handler/dailypatientmeal.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/hadihalimm/sigizi-rsam/internal/api/request"
+	"github.com/hadihalimm/sigizi-rsam/internal/config"
 	"github.com/hadihalimm/sigizi-rsam/internal/repo"
 	"github.com/hadihalimm/sigizi-rsam/internal/service"
 )
@@ -21,18 +22,23 @@ func NewDailyPatientMealHandler(dailyPatientMealService service.DailyPatientMeal
 }
 
 func (h *DailyPatientMealHandler) Create(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	var request request.CreateDailyPatientMeal
 	if err := c.ShouldBindBodyWithJSON(&request); err != nil {
+		logger.Warnw("Bad request", "err", err.Error())
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
 
 	meal, err := h.dailyPatientMealService.Create(request)
 	if err != nil {
+		logger.Errorw("Failed to create daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	logger.Infow("Daily patient meal created successfully", "objectID", meal.ID)
 	c.JSON(http.StatusCreated, gin.H{
 		"message": "Daily patient meal created successfully",
 		"data":    meal,
@@ -40,8 +46,11 @@ func (h *DailyPatientMealHandler) Create(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) GetAll(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	meals, err := h.dailyPatientMealService.GetAll()
 	if err != nil {
+		logger.Errorw("Failed to retrieve all daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -53,14 +62,18 @@ func (h *DailyPatientMealHandler) GetAll(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) GetByID(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	idUint64, _ := strconv.ParseUint(c.Param("id"), 10, 64)
 	id := uint(idUint64)
 	meal, err := h.dailyPatientMealService.GetByID(id)
 	if err != nil {
+		logger.Errorw("Failed to retrieve daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	logger.Infow("Daily patient meal retrieved successfully", "objectID", meal.ID)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Daily patient meal retrieved successfully",
 		"data":    meal,
@@ -68,8 +81,11 @@ func (h *DailyPatientMealHandler) GetByID(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) Update(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	var request request.UpdateDailyPatientMeal
 	if err := c.ShouldBindBodyWithJSON(&request); err != nil {
+		logger.Warnw("Bad request", "err", err.Error())
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -78,10 +94,12 @@ func (h *DailyPatientMealHandler) Update(c *gin.Context) {
 	id := uint(idUint64)
 	meal, err := h.dailyPatientMealService.Update(id, request)
 	if err != nil {
+		logger.Errorw("Failed to updated daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	logger.Infow("Daily patient meal updated successfully", "objectID", meal.ID)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Daily patient meal retrieved successfully",
 		"data":    meal,
@@ -89,23 +107,30 @@ func (h *DailyPatientMealHandler) Update(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) Delete(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	idUint64, _ := strconv.ParseUint(c.Param("id"), 10, 64)
 	id := uint(idUint64)
 	err := h.dailyPatientMealService.Delete(id)
 	if err != nil {
+		logger.Errorw("Failed to delete daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	logger.Infow("Daily patient meal deleted successfully", "objectID", id)
 	c.JSON(http.StatusOK, gin.H{
 		"message": "Daily patient meal deleted successfully",
 	})
 }
 
 func (h *DailyPatientMealHandler) FilterByDateAndRoomType(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	dateString := c.Query("date")
 	date, err := time.Parse("2006-01-02", dateString)
 	if err != nil {
+		logger.Warnw("Bad request", "err", err.Error())
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -114,10 +139,12 @@ func (h *DailyPatientMealHandler) FilterByDateAndRoomType(c *gin.Context) {
 
 	meals, err := h.dailyPatientMealService.FilterByDateAndRoomType(date, roomType)
 	if err != nil {
+		logger.Errorw("Failed to filter daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	logger.Infow("Daily patient meals filtered successfully", "objectCount", len(meals))
 	c.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("Daily patient meals for RoomType %d %s retrieved successfully", roomType, dateString),
 		"data":    meals,
@@ -125,9 +152,12 @@ func (h *DailyPatientMealHandler) FilterByDateAndRoomType(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) CountByDateAndRoomType(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	dateString := c.Query("date")
 	date, err := time.Parse("2006-01-02", dateString)
 	if err != nil {
+		logger.Warnw("Bad request", "err", err.Error())
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
@@ -136,6 +166,7 @@ func (h *DailyPatientMealHandler) CountByDateAndRoomType(c *gin.Context) {
 
 	matrix, err := h.dailyPatientMealService.CountByDateAndRoomType(date, roomType)
 	if err != nil {
+		logger.Errorw("Failed to count daily patient meal", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
@@ -143,6 +174,7 @@ func (h *DailyPatientMealHandler) CountByDateAndRoomType(c *gin.Context) {
 		matrix = []repo.MealMatrixEntry{}
 	}
 
+	logger.Infow("Daily patient meals counted successfully", "objectCount", len(matrix))
 	c.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("Count Daily patient meals for RoomType %d %s retrieved successfully", roomType, dateString),
 		"data":    matrix,
@@ -150,6 +182,8 @@ func (h *DailyPatientMealHandler) CountByDateAndRoomType(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) ExportToExcel(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	dateString := c.Query("date")
 	date, err := time.Parse("2006-01-02", dateString)
 	if err != nil {
@@ -159,6 +193,7 @@ func (h *DailyPatientMealHandler) ExportToExcel(c *gin.Context) {
 
 	file, err := h.dailyPatientMealService.ExportToExcel(date)
 	if err != nil {
+		logger.Errorw("Failed to export daily patient meal to spreadsheet", "error", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to export Excel"})
 		return
 	}
@@ -169,26 +204,30 @@ func (h *DailyPatientMealHandler) ExportToExcel(c *gin.Context) {
 	c.Header("Content-Transfer-Encoding", "binary")
 
 	if err := file.Write(c.Writer); err != nil {
+		logger.Errorw("Failed to write file to response", "error", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to write file to response"})
 	}
 }
 
 func (h *DailyPatientMealHandler) FilterLogsByDate(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	dateString := c.Query("date")
 	date, err := time.Parse("2006-01-02", dateString)
 	if err != nil {
+		logger.Warnw("Bad request", "err", err.Error())
 		c.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	// roomTypeUint64, _ := strconv.ParseUint(c.Query("roomType"), 10, 64)
-	// roomType := uint(roomTypeUint64)
 
 	logs, err := h.dailyPatientMealService.FilterLogsByDate(date)
 	if err != nil {
+		logger.Errorw("Failed to filter daily patient meal logs", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
+	logger.Infow("Daily patient meals counted successfully", "objectCount", len(logs))
 	c.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("Daily patient meals log for %s retrieved successfully", dateString),
 		"data":    logs,
@@ -196,6 +235,8 @@ func (h *DailyPatientMealHandler) FilterLogsByDate(c *gin.Context) {
 }
 
 func (h *DailyPatientMealHandler) CountDietCombinationsByDate(c *gin.Context) {
+	logger := config.WithRequestContext(config.Logger, c)
+
 	dateString := c.Query("date")
 	date, err := time.Parse("2006-01-02", dateString)
 	if err != nil {
@@ -208,9 +249,11 @@ func (h *DailyPatientMealHandler) CountDietCombinationsByDate(c *gin.Context) {
 		nonComplicationCount,
 		err := h.dailyPatientMealService.CountDietCombinationsByDate(date)
 	if err != nil {
+		logger.Errorw("Failed to count diet combinations", "error", err.Error())
 		c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
+	logger.Infow("Diet combinations count retrieved successfully", "objectCount", len(dietCombinationsCount))
 	c.JSON(http.StatusOK, gin.H{
 		"message": fmt.Sprintf("Diet combinations count for %s retrieved successfully", dateString),
 		"data": gin.H{
