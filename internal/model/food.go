459a2130eb41eb9b88999c631650c6cca0f4b0a9
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-18 14:54:25 +0700

    update model

--- internal/model/food.go
+++ internal/model/food.go
@@ -2,10 +2,16 @@ package model
 
 import "time"
 
-type MealItem struct {
-	ID        uint      `gorm:"primaryKey" json:"id"`
-	Name      string    `gorm:"not null" json:"name"`
-	Quantity  float64   `gorm:"not null" json:"quantity"`
-	CreatedAt time.Time `json:"createdAt"`
-	UpdatedAt time.Time `json:"updatedAt"`
+type Food struct {
+	ID            uint           `gorm:"primaryKey" json:"id"`
+	Name          string         `gorm:"not null" json:"name"`
+	FoodMaterials []FoodMaterial `gorm:"many2many:food_material_usages;" json:"foodMaterials"`
+	CreatedAt     time.Time      `json:"createdAt"`
+	UpdatedAt     time.Time      `json:"updatedAt"`
+}
+
+type FoodMaterialUsage struct {
+	FoodID         uint    `gorm:"primaryKey" json:"foodID"`
+	FoodMaterialID uint    `gorm:"primaryKey" json:"foodMaterialID"`
+	QuantityUsed   float64 `gorm:"not null" json:"quantityUsed"`
 }
