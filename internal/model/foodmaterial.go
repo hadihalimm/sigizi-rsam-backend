b7ec0c2b4f893b17472a7f57b3d87b7307cb90a4
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-20 08:44:57 +0700

    change json label

--- internal/model/foodmaterial.go
+++ internal/model/foodmaterial.go
@@ -4,5 +4,5 @@ type FoodMaterial struct {
 	ID              uint    `gorm:"primaryKey" json:"id"`
 	Name            string  `gorm:"not null" json:"name"`
 	Unit            string  `gorm:"not null" json:"unit"`
-	StandardPerMeal float64 `gorm:"not null" json:"standard_per_unit"`
+	StandardPerMeal float64 `gorm:"not null" json:"standardPerMeal"`
 }
