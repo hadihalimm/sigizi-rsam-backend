84e1c45d917b2e75e629f3d40289ff3da4a0c489
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-06-23 21:11:31 +0700

    change input type to uint

--- internal/api/request/mealmenu.go
+++ internal/api/request/mealmenu.go
@@ -2,7 +2,7 @@ package request
 
 type CreateMealMenu struct {
 	Name       string `json:"name" binding:"required" validate:"required"`
-	Day        string `json:"day" binding:"required" validate:"required,oneof=senin selasa rabu kamis jumat sabtu minggu"`
+	Day        uint   `json:"day" binding:"required" validate:"required"`
 	Time       string `json:"time" binding:"required" validate:"required,oneof=pagi siang sore"`
 	MealTypeID uint   `json:"mealTypeID" binding:"required" validate:"required"`
 	FoodIDs    []uint `json:"foodIDs" validate:"required,min=1,dive,gt=0"`
@@ -10,7 +10,7 @@ type CreateMealMenu struct {
 
 type UpdateMealMenu struct {
 	Name       string `json:"name" binding:"required" validate:"required"`
-	Day        string `json:"day" binding:"required" validate:"required,oneof=senin selasa rabu kamis jumat sabtu minggu"`
+	Day        uint   `json:"day" binding:"required" validate:"required"`
 	Time       string `json:"time" binding:"required" validate:"required,oneof=pagi siang sore"`
 	MealTypeID uint   `json:"mealTypeID" binding:"required" validate:"required"`
 	FoodIDs    []uint `json:"foodIDs" validate:"required,min=1,dive,gt=0"`
--- internal/model/mealmenu.go
+++ internal/model/mealmenu.go
@@ -3,13 +3,20 @@ package model
 import "time"
 
 type MealMenu struct {
-	ID         uint      `gorm:"primaryKey" json:"id"`
-	Name       string    `gorm:"not null" json:"name"`
-	Day        string    `gorm:"not null" json:"day"`
-	Time       string    `gorm:"not null" json:"time"`
-	Foods      []Food    `gorm:"many2many:meal_foods;" json:"foods"`
-	MealTypeID uint      `json:"mealTypeID"`
-	MealType   MealType  `json:"mealType"`
-	CreatedAt  time.Time `json:"createdAt"`
-	UpdatedAt  time.Time `json:"updatedAt"`
+	ID                 uint      `gorm:"primaryKey" json:"id"`
+	Name               string    `gorm:"not null;uniqueIndex:idx_mealmenu" json:"name"`
+	Day                uint      `gorm:"not null;uniqueIndex:idx_mealmenu" json:"day"`
+	Time               string    `gorm:"not null;uniqueIndex:idx_mealmenu" json:"time"`
+	MealTypeID         uint      `gorm:"not null;uniqueIndex:idx_mealmenu" json:"mealTypeID"`
+	Foods              []Food    `gorm:"many2many:meal_foods;" json:"foods"`
+	MealType           MealType  `json:"mealType"`
+	CreatedAt          time.Time `json:"createdAt"`
+	UpdatedAt          time.Time `json:"updatedAt"`
+	MealMenuTemplateID uint      `gorm:"not null" json:"mealMenuTemplateID"`
+}
+
+type MealMenuTemplate struct {
+	ID        uint       `gorm:"primaryKey" json:"id"`
+	Name      string     `gorm:"unique;not null" json:"name"`
+	MealMenus []MealMenu `json:"mealMenus"`
 }
