cca83606bcf0e71ece465352a5f5aff0df0d7571
Hadi Halim Kamil <hadihalimkamil@gmail.com>
2025-07-01 10:59:08 +0700

    move FoodMaterial

--- internal/model/foodmaterial.go
+++
@@ -1,8 +0,0 @@
-package model
-
-type FoodMaterial struct {
-	ID              uint    `gorm:"primaryKey" json:"id"`
-	Name            string  `gorm:"not null" json:"name"`
-	Unit            string  `gorm:"not null" json:"unit"`
-	StandardPerMeal float64 `gorm:"not null" json:"standardPerMeal"`
-}
